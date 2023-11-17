40ec3c2d4a2e313e015a09e79685d3b6f64bb3ba
Ben Rugg <ben@theregime.com>
2023-11-17 12:02:11 -0600

    Add basic Three, HTML, scrolling structure

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { Canvas } from "@react-three/fiber"
-import { Environment, Preload, OrbitControls } from "@react-three/drei"
+import { Environment, Preload, OrbitControls, ScrollControls, Scroll } from "@react-three/drei"
 
 import dynamic from "next/dynamic"
 
@@ -9,21 +9,34 @@ const Test = dynamic(() => import("@/app/components/Test"), { ssr: false })
 
 export default function Home() {
   return (
-    <Canvas
-      style={{
-        position: "fixed",
-        top: 0,
-        left: 0,
-        width: "100vw",
-        height: "100vh",
-      }}
-      shadows={"soft"}
-      camera={{ position: [6, 4, -12], fov: 50 }}
-    >
-      <Test />
-      <Environment files="/images/polyhaven-aerodynamics_workshop_1k.hdr" />
-      <OrbitControls makeDefault />
-      <Preload all />
-    </Canvas>
+    <>
+      <Canvas
+        style={{
+          position: "fixed",
+          top: 0,
+          left: 0,
+          width: "100vw",
+          height: "100vh",
+        }}
+        shadows={"soft"}
+        camera={{ position: [0, 0, -5], fov: 50 }}
+      >
+        <Environment files="/images/polyhaven-aerodynamics_workshop_1k.hdr" />
+        <color attach="background" args={["#f0d5ff"]} />
+        {/* <OrbitControls makeDefault scale={1} /> */}
+        <Preload all />
+        <ScrollControls damping={0.1} pages={10} distance={0.5}>
+          <Scroll>
+            <Test />
+          </Scroll>
+          <Scroll html>
+            <div className="flex flex-col items-center justify-center min-h-screen w-screen absolute">
+              <h1 className="text-6xl font-bold">Hello World</h1>
+              <p className="mt-3 text-2xl">Welcome to the test portfolio.</p>
+            </div>
+          </Scroll>
+        </ScrollControls>
+      </Canvas>
+    </>
   )
 }
--- src/app/components/Test.tsx
+++ src/app/components/Test.tsx
@@ -2,27 +2,40 @@
 
 import * as THREE from "three"
 import { useRef, useState } from "react"
-import { useFrame, ThreeElements } from "@react-three/fiber"
+import { useFrame, useThree, ThreeElements } from "@react-three/fiber"
 import { Backdrop, SoftShadows } from "@react-three/drei"
 import { useControls } from "leva"
 
+const numParticles = 300
+const zMin = 0
+const zMax = 10
+const overshootScreenScale = 2
 const rotationSpeed = 0.5
 
-function Box(props: ThreeElements["mesh"]) {
+function Particle(props: ThreeElements["mesh"] & { targetPosition: [x: number, y: number, z: number] }) {
   const ref = useRef<THREE.Mesh>(null!)
   const [hovered, hover] = useState(false)
   const [clicked, click] = useState(false)
+
+  const { width, height } = useThree((state) => state.viewport)
 
   useFrame((state, delta) => {
     ref.current.rotation.x += delta * rotationSpeed
     if (hovered) ref.current.rotation.y += delta * rotationSpeed * 2
   })
 
+  const position: [x: number, y: number, z: number] = [
+    (props.targetPosition[0] * width - width / 2) * overshootScreenScale,
+    (props.targetPosition[1] * height - height / 2) * overshootScreenScale,
+    props.targetPosition[2],
+  ]
+
   return (
     <mesh
       {...props}
+      position={position}
       ref={ref}
-      scale={clicked ? 1.5 : 1}
+      scale={clicked ? 0.3 : 0.2}
       onClick={(event) => click(!clicked)}
       onPointerOver={(event) => hover(true)}
       onPointerOut={(event) => hover(false)}
@@ -33,45 +46,55 @@ function Box(props: ThreeElements["mesh"]) {
   )
 }
 
-const Light = () => {
-  const directionalCtl = useControls("Directional Light", {
-    position: {
-      x: -1,
-      y: 3,
-      z: -4.4,
-    },
-    intensity: 4.0,
-    castShadow: true,
-  })
+// const Light = () => {
+//   const directionalCtl = useControls("Directional Light", {
+//     position: {
+//       x: -1,
+//       y: 3,
+//       z: -4.4,
+//     },
+//     intensity: 4.0,
+//     castShadow: true,
+//   })
 
-  return (
-    <directionalLight
-      position={[directionalCtl.position.x, directionalCtl.position.y, directionalCtl.position.z]}
-      intensity={directionalCtl.intensity}
-      castShadow={directionalCtl.castShadow}
-    />
-  )
-}
+//   return (
+//     <directionalLight
+//       position={[directionalCtl.position.x, directionalCtl.position.y, directionalCtl.position.z]}
+//       intensity={directionalCtl.intensity}
+//       castShadow={directionalCtl.castShadow}
+//     />
+//   )
+// }
 
 export default function Test() {
+  const randomPositions = () => {
+    const positions: [x: number, y: number, z: number][] = []
+    for (let i = 0; i < numParticles; i++) {
+      positions.push([Math.random(), Math.random(), THREE.MathUtils.randFloat(zMin, zMax)])
+    }
+    return positions
+  }
+
   return (
     <>
-      <color attach="background" args={["#eeccff"]} />
       <ambientLight intensity={0.5} />
-      <Light />
-      <Box position={[-1.2, 0, 0]} castShadow />
-      <Box position={[1.2, 0, 0]} castShadow />
-      <Backdrop
+      {/* <Light /> */}
+      {randomPositions().map((position, index) => (
+        <Particle key={index} targetPosition={position} castShadow />
+      ))}
+      {/* <Particle position={[-1.2, 0, 20]} castShadow /> */}
+      {/* <Particle position={[1.2, 0, 20]} castShadow /> */}
+      {/* <Backdrop
         floor={1.4} // Stretches the floor segment, 0.25 by default
         segments={20} // Mesh-resolution, 20 by default
         receiveShadow={true} // Whether to receive shadows, false by default
         scale={[20, 10, 5]}
-        position={[0, -1, 2]}
+        position={[0, -1, 22]}
         rotation={[0, Math.PI, 0]}
       >
         <meshStandardMaterial roughness={0.7} color="#eeeeff" side={THREE.DoubleSide} />
-      </Backdrop>
-      <SoftShadows />
+      </Backdrop> */}
+      {/* <SoftShadows /> */}
     </>
   )
 }
