85893b1613b42d55340293ab29f99f8be0f8eb2f
Ben Rugg <ben@theregime.com>
2023-11-17 12:27:08 -0600

    Refactor positioning of particles

--- src/app/components/Test.tsx
+++ src/app/components/Test.tsx
@@ -1,7 +1,7 @@
 "use client"
 
 import * as THREE from "three"
-import { useRef, useState } from "react"
+import { useMemo, useRef, useState } from "react"
 import { useFrame, useThree, ThreeElements } from "@react-three/fiber"
 // import { useControls } from "leva"
 
@@ -11,28 +11,19 @@ const zMax = 10
 const overshootScreenScale = 2
 const rotationSpeed = 0.5
 
-function Particle(props: ThreeElements["mesh"] & { targetPosition: [x: number, y: number, z: number] }) {
+function Particle(props: ThreeElements["mesh"]) {
   const ref = useRef<THREE.Mesh>(null!)
   const [hovered, hover] = useState(false)
   const [clicked, click] = useState(false)
-
-  const { width, height } = useThree((state) => state.viewport)
 
   useFrame((state, delta) => {
     ref.current.rotation.x += delta * rotationSpeed
     if (hovered) ref.current.rotation.y += delta * rotationSpeed * 2
   })
 
-  const position: [x: number, y: number, z: number] = [
-    (props.targetPosition[0] * width - width / 2) * overshootScreenScale,
-    (props.targetPosition[1] * height - height / 2) * overshootScreenScale,
-    props.targetPosition[2],
-  ]
-
   return (
     <mesh
       {...props}
-      position={position}
       ref={ref}
       scale={clicked ? 0.3 : 0.2}
       onClick={(event) => click(!clicked)}
@@ -55,23 +46,29 @@ function Temp(props: ThreeElements["mesh"]) {
 }
 
 export default function Test() {
-  const randomPositions = () => {
+  const randomParticlePositions = useMemo(() => {
     const positions: [x: number, y: number, z: number][] = []
     for (let i = 0; i < numParticles; i++) {
       positions.push([Math.random(), Math.random(), THREE.MathUtils.randFloat(zMin, zMax)])
     }
     return positions
-  }
+  }, [])
 
-  const { height } = useThree((state) => state.viewport)
+  const { width, height } = useThree((state) => state.viewport)
 
   return (
     <>
       <ambientLight intensity={0.5} />
 
-      {randomPositions().map((position, index) => (
-        <Particle key={index} targetPosition={position} castShadow />
-      ))}
+      {randomParticlePositions.map((randomPosition, index) => {
+        const position: [x: number, y: number, z: number] = [
+          (randomPosition[0] * width - width / 2) * overshootScreenScale,
+          (randomPosition[1] * height - height / 2) * overshootScreenScale,
+          randomPosition[2],
+        ]
+
+        return <Particle key={index} position={position} castShadow />
+      })}
 
       <Temp position={[0, -height * 1, 0]} />
       <Temp position={[0, -height * 2, 2]} />
