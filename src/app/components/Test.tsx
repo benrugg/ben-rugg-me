f238f2368bc73d8137f66077ae9f1a38ceb85bc5
Ben Rugg <ben@theregime.com>
2023-11-17 14:17:22 -0600

    Add rotation based on pointer

--- src/app/components/Test.tsx
+++ src/app/components/Test.tsx
@@ -1,6 +1,7 @@
 "use client"
 
 import * as THREE from "three"
+import { MathUtils } from "three"
 import { useMemo, useRef, useState } from "react"
 import { useFrame, useThree, ThreeElements } from "@react-three/fiber"
 import { Instance, Instances } from "@react-three/drei"
@@ -10,7 +11,9 @@ const numParticles = 300
 const zMin = 0
 const zMax = 10
 const overshootScreenScale = 2
-const rotationSpeed = 0.5
+const particleRotationSpeed = 0.5
+const groupRotationAmount = 0.02
+const groupRotationDamping = 2.75
 
 function Particle(props: { position: [x: number, y: number, z: number] }) {
   const ref = useRef<THREE.InstancedMesh>(null!)
@@ -19,8 +22,8 @@ function Particle(props: { position: [x: number, y: number, z: number] }) {
   const [clicked, setClicked] = useState(false)
 
   useFrame((state, delta) => {
-    ref.current.rotation.x += delta * rotationSpeed
-    if (hovered) ref.current.rotation.y += delta * rotationSpeed * 2
+    ref.current.rotation.x += delta * particleRotationSpeed
+    if (hovered) ref.current.rotation.y += delta * particleRotationSpeed * 2
     ref.current.scale.x = ref.current.scale.y = ref.current.scale.z = THREE.MathUtils.lerp(ref.current.scale.z, clicked ? 0.5 : 0.2, 0.1)
     // @ts-ignore: .color is not in the type definition
     ref.current.color.set(hovered ? "hotpink" : "orange")
@@ -52,31 +55,49 @@ export default function Test() {
   const randomParticlePositions = useMemo(() => {
     const positions: [x: number, y: number, z: number][] = []
     for (let i = 0; i < numParticles; i++) {
-      positions.push([Math.random(), Math.random(), THREE.MathUtils.randFloat(zMin, zMax)])
+      positions.push([Math.random(), Math.random(), MathUtils.randFloat(zMin, zMax)])
     }
     return positions
   }, [])
 
+  const containerRef = useRef<THREE.Group>(null!)
   const { width, height } = useThree((state) => state.viewport)
+
+  useFrame((state, delta) => {
+    containerRef.current.rotation.y = MathUtils.damp(
+      containerRef.current.rotation.y,
+      state.pointer.x * Math.PI * groupRotationAmount,
+      groupRotationDamping,
+      delta,
+    )
+    containerRef.current.rotation.x = MathUtils.damp(
+      containerRef.current.rotation.x,
+      state.pointer.y * Math.PI * groupRotationAmount * 0.5,
+      groupRotationDamping,
+      delta,
+    )
+  })
 
   return (
     <>
       <ambientLight intensity={0.5} />
 
-      <Instances range={randomParticlePositions.length}>
-        <icosahedronGeometry args={[1, 0]} />
-        <meshPhysicalMaterial roughness={0.12} reflectivity={0.7} color={"white"} />
+      <group ref={containerRef}>
+        <Instances range={randomParticlePositions.length}>
+          <icosahedronGeometry args={[1, 0]} />
+          <meshPhysicalMaterial roughness={0.12} reflectivity={0.7} color={"white"} />
 
-        {randomParticlePositions.map((randomPosition, index) => {
-          const position: [x: number, y: number, z: number] = [
-            (randomPosition[0] * width - width / 2) * overshootScreenScale,
-            (randomPosition[1] * height - height / 2) * overshootScreenScale,
-            randomPosition[2],
-          ]
+          {randomParticlePositions.map((randomPosition, index) => {
+            const position: [x: number, y: number, z: number] = [
+              (randomPosition[0] * width - width / 2) * overshootScreenScale,
+              (randomPosition[1] * height - height / 2) * overshootScreenScale,
+              randomPosition[2],
+            ]
 
-          return <Particle key={index} position={position} />
-        })}
-      </Instances>
+            return <Particle key={index} position={position} />
+          })}
+        </Instances>
+      </group>
 
       <Temp position={[0, -height * 1, 0]} />
       <Temp position={[0, -height * 2, 2]} />
