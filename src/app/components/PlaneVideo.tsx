dcea2c39efa6c74b64f1e76bb464eee034bbf493
Ben Rugg <ben@theregime.com>
2023-12-07 17:05:37 -0600

    Add initial structure for pausing video textures

--- src/app/components/PlaneVideo.tsx
+++ src/app/components/PlaneVideo.tsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react"
 import { useVideoTexture } from "@react-three/drei"
 
 const aspectRatio = 16 / 9
@@ -10,6 +11,13 @@ export default function PlaneVideo(props: { url: string | undefined }) {
 
   // load the video
   const videoTexture = useVideoTexture(props.url)
+
+  // pause the video on unmount or change of video
+  useEffect(() => {
+    return () => {
+      videoTexture.image.pause()
+    }
+  }, [videoTexture])
 
   return (
     <mesh position={[0, 0, 0]}>
