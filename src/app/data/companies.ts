d59c9550d1f786f6ce97af3908dc6d1defac337e
Ben Rugg <ben@theregime.com>
2023-12-13 14:15:48 -0600

    Add real company content

--- src/app/data/companies.ts
+++ src/app/data/companies.ts
@@ -57,6 +57,25 @@ export const companyInfo: Content[] = [
         title: "/Company",
         body: "Echo / 2014-2022",
       },
+      {
+        title: "/Website",
+        body: "echoprayer.com",
+        url: "https://echoprayer.com",
+      },
+      {
+        title: "/Description",
+        body: "Echo is an iOS and Android app made for people who pray. Collaborating with another skilled developer, my business partner and I created the app and then grew it to nearly 700k users. We built a suite of features while maintaining a simple and intuitive user experience. We sold Echo in December, 2022.",
+      },
+      {
+        title: "/Highlights",
+        body: `
+          <ul>
+            <li>Created an app with ~700k users</li>
+            <li>4.8 rating in the App Store</li>
+            <li>Sold Echo in 2022</li>
+          </ul>
+        `,
+      },
     ],
     slides: [
       {
@@ -94,6 +113,19 @@ export const companyInfo: Content[] = [
         title: "/Company",
         body: "Revolver / 2016",
       },
+      {
+        title: "/Description",
+        body: "Revolver was a high-end subscription art service. My business partners and I created the brand, website, and product from scratch. We designed and manufactured custom frames and printed art, and spun up a subscription service quickly.",
+      },
+      {
+        title: "/Highlights",
+        body: `
+          <ul>
+            <li>Created a high-end custom manufactured product</li>
+            <li>Spun up marketing and distribution quickly</li>
+          </ul>
+        `,
+      },
     ],
     slides: [
       {
@@ -127,6 +159,25 @@ export const companyInfo: Content[] = [
       {
         title: "/Company",
         body: "Kingdom / 2016-2017",
+      },
+      {
+        title: "/Website",
+        body: "kingdomindustry.com",
+        url: "https://kingdomindustry.com/",
+      },
+      {
+        title: "/Description",
+        body: "Kingdom is an architecture and design firm. I invested in this small company and came on board to help scale operations. We created a new brand and website, while also building out a new office space. I helped them grow from 2 to 8 employees in 3 months. We developed properties in the Hollywood Hills and designed a 5,000 sq ft home in Beverly Hills, while simultaneously doing corporate design work and spinning up new potential consumer products.",
+      },
+      {
+        title: "/Highlights",
+        body: `
+          <ul>
+            <li>Developed properties in the Hollywood Hills</li>
+            <li>Designed a 5,000 sq ft home in Beverly Hills</li>
+            <li>Grew operations from 2 to 8 employees</li>
+          </ul>
+        `,
       },
     ],
     slides: [
@@ -168,6 +219,26 @@ export const companyInfo: Content[] = [
         title: "/Company",
         body: "Clover / 2008-2015",
       },
+      {
+        title: "/Website",
+        body: "cloversites.com",
+        url: "https://www.cloversites.com/",
+      },
+      {
+        title: "/Description",
+        body: "Clover was my first major startup experience. My business partner and I created a website builder for churches and non-profits, which quickly became the industry leader. We grew the company to 20 employees and 15,000 customers, while creating new versions of our revolutionary product. In 2012, we launched a payment processor, Clover Donations, which processed over $500 million. We sold Clover in December, 2015 for a 10x multiple.",
+      },
+      {
+        title: "/Highlights",
+        body: `
+          <ul>
+            <li>Created a revolutionary website builder</li>
+            <li>Created an enterprise-scale payment processor</li>
+            <li>Scaled to 20 employees and $5m+ ARR</li>
+            <li>Sold Clover in 2015 for a 10x multiple</li>
+          </ul>
+        `,
+      },
     ],
     slides: [
       {
@@ -201,6 +272,24 @@ export const companyInfo: Content[] = [
       {
         title: "/Company",
         body: "The Regime / 2005-2008",
+      },
+      {
+        title: "/Website",
+        body: "theregime.com",
+        url: "https://www.theregime.com/",
+      },
+      {
+        title: "/Description",
+        body: "The Regime was a web, print and branding agency that I started with my now-longtime business partner. We created over 70 unique interactive websites, brand identities, and print materials for a variety of clients, from bands and film companies to restaraunts and car dealerships. Our work was often animated and interactive, and we were known for our unique, punchy style, and our extremely rapid development speed.",
+      },
+      {
+        title: "/Highlights",
+        body: `
+          <ul>
+            <li>Created 70+ unique, interactive experiences</li>
+            <li>Designed and delivered a very wide range of projects and brands</li>
+          </ul>
+        `,
       },
     ],
     slides: [
