26ae593599a81f10bd03e044ca432b9173b77fd1
Ben Rugg <ben@theregime.com>
2023-12-13 19:38:44 -0600

    Add basic title and meta description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next"
 import "@/app/styles/globals.css"
 
 export const metadata: Metadata = {
-  title: "Test Portfolio",
-  description: "The portfolio of Test",
+  title: "Ben Rugg // Full-Stack Engineer & Creative Entrepreneur",
+  description: "Digital portfolio of Ben Rugg, a creative entrepreneur and full-stack engineer in Madison, Wisconsin.",
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
