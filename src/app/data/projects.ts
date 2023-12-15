203bfd3969022dbf732ae27bb6a6984fb42ac72a
Ben Rugg <ben@theregime.com>
2023-12-15 12:51:39 -0600

    Add some of the real project content

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -56,4 +56,153 @@ export const projectInfo: Content[] = [
       },
     ],
   },
+  {
+    text: [
+      {
+        title: "/Project",
+        body: "Daily Good / 2023",
+      },
+      {
+        title: "/Description",
+        body: "Daily Good is a unique physical and digital product delivering a daily dose of positivity. I created the a high-touch custom onboarding flow and an admin app powered by ChatGPT. I also created the front-end marketing site and collaborated with my business partner to manufacture the physical product.",
+      },
+      {
+        title: "/Website",
+        body: "dailygood.us",
+        url: "https://dailygood.us/",
+      },
+      {
+        title: "/Tech Stack",
+        body: `
+          <ul>
+            <li>Vue.js</li>
+            <li>Firebase</li>
+            <li>Node.js</li>
+            <li>ChatGPT AI</li>
+          </ul>
+        `,
+      },
+    ],
+    slides: [
+      {
+        image: "/images/content/projects/daily-good-0.jpg",
+      },
+      {
+        image: "/images/content/projects/daily-good-1.jpg",
+      },
+      {
+        image: "/images/content/projects/daily-good-2.jpg",
+      },
+      {
+        image: "/images/content/projects/daily-good-3.jpg",
+      },
+      {
+        image: "/images/content/projects/daily-good-4.jpg",
+      },
+      {
+        image: "/images/content/projects/daily-good-5.jpg",
+      },
+      {
+        image: "/images/content/projects/daily-good-6.jpg",
+      },
+      {
+        image: "/images/content/projects/daily-good-7.jpg",
+      },
+      {
+        image: "/images/content/projects/daily-good-8.jpg",
+      },
+    ],
+  },
+  {
+    text: [
+      {
+        title: "/Project",
+        body: "Saigo / 2023",
+      },
+      {
+        title: "/Description",
+        body: "Saigo was a mobile game concept invented by my 10-year-old nephew. I collaborated with my nephews, my brother and a designer to create several prototype iterations. We made two AI game engines which could demolish a human player.",
+      },
+      {
+        title: "/Tech Stack",
+        body: `
+          <ul>
+            <li>React.js</li>
+            <li>Vue.js</li>
+            <li>Expo</li>
+            <li>Quasar</li>
+            <li>JavaScript Web Workers</li>
+          </ul>
+        `,
+      },
+    ],
+    slides: [
+      {
+        image: "/images/content/projects/saigo-0.jpg",
+      },
+      {
+        image: "/images/content/projects/saigo-1.jpg",
+      },
+      {
+        image: "/images/content/projects/saigo-2.jpg",
+      },
+      {
+        image: "/images/content/projects/saigo-3.jpg",
+      },
+      {
+        image: "/images/content/projects/saigo-4.jpg",
+      },
+      {
+        image: "/images/content/projects/saigo-5.jpg",
+      },
+    ],
+  },
+  {
+    text: [
+      {
+        title: "/Project",
+        body: "AI Render / 2022-2023",
+      },
+      {
+        title: "/Description",
+        body: "AI Render is an open-source add-on for Blender I created right at the beginning of the generative art craze. It harnesses Stable Diffusion AI to create beautiful versions of 3D scenes, empowering artists to iterate more in less time. It's been downloaded over 50,000 times and is used by artists all over the world.",
+      },
+      {
+        title: "/Code",
+        body: "github.com/benrugg/AI-Render",
+        url: "https://github.com/benrugg/AI-Render",
+      },
+      {
+        title: "/Tech Stack",
+        body: `
+          <ul>
+            <li>Python</li>
+            <li>AWS Lambda</li>
+            <li>Stable Diffusion AI</li>
+            <li>REST API Gateway</li>
+          </ul>
+        `,
+      },
+    ],
+    slides: [
+      {
+        image: "/images/content/projects/ai-render-0.jpg",
+      },
+      {
+        video: "/video/ai-render.mp4",
+      },
+      {
+        image: "/images/content/projects/ai-render-2.jpg",
+      },
+      {
+        image: "/images/content/projects/ai-render-3.jpg",
+      },
+      {
+        image: "/images/content/projects/ai-render-4.jpg",
+      },
+      {
+        image: "/images/content/projects/ai-render-5.jpg",
+      },
+    ],
+  },
 ]
