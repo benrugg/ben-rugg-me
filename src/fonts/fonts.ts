655a3e875ab1ae3e50d00bb3c1a8a15bcb5ee667
Ben Rugg <ben@theregime.com>
2023-12-18 17:03:27 -0600

    Set fonts to load without swapping

--- src/fonts/fonts.ts
+++ src/fonts/fonts.ts
@@ -1,3 +1,3 @@
 import { Fira_Code } from "next/font/google"
 
-export const firaCode = Fira_Code({ weight: ["300", "400", "700"], subsets: ["latin"] })
+export const firaCode = Fira_Code({ weight: ["300", "400", "700"], subsets: ["latin"], display: "block" })
