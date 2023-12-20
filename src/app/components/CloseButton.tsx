56ced2615fe57ffa234d11f7c56973c442781435
Ben Rugg <ben@theregime.com>
2023-12-20 11:21:04 -0600

    Extract close x icon to separate file

--- src/app/components/CloseButton.tsx
+++ src/app/components/CloseButton.tsx
@@ -1,17 +1,6 @@
+import Image from "next/image"
+import closeIcon from "@/assets/icons/close-x-icon.svg"
+
 export default function CloseButton(props: { onClick: () => void }) {
-  return (
-    <svg
-      className="cursor-pointer p-4 box-content"
-      xmlns="http://www.w3.org/2000/svg"
-      width="32"
-      height="32"
-      viewBox="0 0 64 64"
-      onClick={props.onClick}
-    >
-      <g fill="#FFFFFF">
-        <path d="M10.3 56.3 8 54 54 8l2.3 2.3-46 46"></path>
-        <path d="m54 56.3-46-46L10.3 8l46 46-2.3 2.3"></path>
-      </g>
-    </svg>
-  )
+  return <Image className="cursor-pointer p-4 box-content" src={closeIcon} alt="close" onClick={props.onClick} />
 }
