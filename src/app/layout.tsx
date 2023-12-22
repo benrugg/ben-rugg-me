e6dfcc126d1fc2bfb045fe8eb2c830aa408b6ce7
Ben Rugg <ben@theregime.com>
2023-12-22 16:11:39 -0600

    Add open graph and sharing image & video

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,32 @@
 import type { Metadata } from "next"
+import { rootUrl } from "@/config/root-url"
 import "@/app/styles/globals.css"
 
 export const metadata: Metadata = {
-  title: "Ben Rugg // Full-Stack Engineer & Creative Entrepreneur",
-  description: "Digital portfolio of Ben Rugg // Serial entrepreneur and full-stack engineer in Madison, Wisconsin.",
+  title: "Ben Rugg // Full-Stack Engineer & Entrepreneur",
+  description: "Digital portfolio of Ben Rugg // Serial entrepreneur and Full-Stack Engineer in Madison, Wisconsin.",
+  openGraph: {
+    title: "Ben Rugg // Full-Stack Engineer & Entrepreneur",
+    description: "Digital portfolio of Ben Rugg // Serial entrepreneur and Full-Stack Engineer in Madison, Wisconsin.",
+    url: rootUrl,
+    images: [
+      {
+        url: `${rootUrl}/og-sharing/main-site-image.jpg`,
+        width: 1600,
+        height: 900,
+      },
+    ],
+    videos: [
+      {
+        url: `${rootUrl}/og-sharing/site-preview.mp4`,
+        width: 1120,
+        height: 630,
+        type: "video/mp4",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
