95f8d7add4837508328e5758179d0609aa40d0cc
Ben Rugg <ben@theregime.com>
2023-12-29 12:26:45 -0600

    Add image to home content for bots

--- src/app/bot-pages/Home.tsx
+++ src/app/bot-pages/Home.tsx
@@ -1,9 +1,17 @@
 import FooterForBot from "./Footer"
+import Image from "next/image"
 
 export default function HomeForBot() {
   return (
     <>
-      <p className="mb-20">Digital portfolio of Ben Rugg, expert full-stack engineer and serial entrepreneur.</p>
+      <p className="lg:mb-6 mb-20 -mt-5">Digital portfolio of Ben Rugg, expert full-stack engineer and serial entrepreneur.</p>
+      <Image
+        src="/images/home/home-screenshot.jpg"
+        alt="Screen of Companies & Products and a Screen of Projects & Software"
+        width={2400}
+        height={1303}
+        className="max-w-[1460px] w-full h-auto mx-auto mb-16"
+      />
       <p className="uppercase tracking-wide text-lg mb-2">View my work:</p>
       <div className="flex justify-center xs:flex-row flex-col xs:space-x-12 space-x-0 xs:space-y-0 space-y-2 text-lg uppercase tracking-wide">
         <p>
